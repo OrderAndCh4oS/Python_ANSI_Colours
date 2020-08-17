4c6eece3093af2654e9554cb2daa4f70ebbdf6d2
Sarcoma <sarcoma@live.co.uk>
2020-08-17 19:04:38 +0100

    Add poetry

---
+++ README.md
@@ -0,0 +1,20 @@
+# Python ANSI Colours
+
+Static methods to wrap text in ANSI Colours.
+
+## Installation
+
+
+Install by running:
+
+`pip install ansi_colours`
+
+## Usage
+
+```
+from ansi_colours import AnsiColours as colour
+
+# Red text
+
+colour.red("I'm red")
+```
--- tests/test_ansi_colours.py
+++ tests/test_ansi_colours.py
@@ -1,20 +1,13 @@
-import unittest
 
-from ansi_colours import AnsiColours as colour
-
-
-class TestAnsiColours(unittest.TestCase):
-
-    def test_methods(self):
-        self.assertEqual(colour.black("black"), "\033[0;30mblack\033[0m")
-        self.assertEqual(colour.blue("blue"), "\033[0;34mblue\033[0m")
-        self.assertEqual(colour.cyan("cyan"), "\033[0;36mcyan\033[0m")
-        self.assertEqual(colour.green("green"), "\033[0;32mgreen\033[0m")
-        self.assertEqual(colour.red("red"), "\033[0;31mred\033[0m")
-        self.assertEqual(colour.purple("purple"), "\033[0;35mpurple\033[0m")
-        self.assertEqual(colour.yellow("yellow"), "\033[0;33myellow\033[0m")
-        self.assertEqual(colour.light_grey("light_grey"), "\033[0;37mlight_grey\033[0m")
+from ansi_colours import AnsiColours
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_methods():
+    assert AnsiColours.black("black") == "\033[0;30mblack\033[0m"
+    assert AnsiColours.blue("blue") == "\033[0;34mblue\033[0m"
+    assert AnsiColours.cyan("cyan") == "\033[0;36mcyan\033[0m"
+    assert AnsiColours.green("green") == "\033[0;32mgreen\033[0m"
+    assert AnsiColours.red("red") == "\033[0;31mred\033[0m"
+    assert AnsiColours.purple("purple") == "\033[0;35mpurple\033[0m"
+    assert AnsiColours.yellow("yellow") == "\033[0;33myellow\033[0m"
+    assert AnsiColours.light_grey("light_grey") == "\033[0;37mlight_grey\033[0m"
